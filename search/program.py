2c111d6e3b97abb1461a55aee32cff73730a0bf6
alex-ware <alexmenzieadh@gmail.com>
2023-03-30 22:46:59 +1100

    working on board update during spread

--- search/program.py
+++ search/program.py
@@ -12,7 +12,6 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
 
     See the specification document for more details.
     """
-
     # The render_board function is useful for debugging -- it will print out a 
     # board state in a human-readable format. Try changing the ansi argument 
     # to True to see a colour-coded version (if your terminal supports it).
@@ -24,7 +23,6 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
     for key, value in board.items():
         if 'r' in value:
             cell = key
-            
     print(board.get(cell))
     
     # extracting coordinates from starting cell
@@ -34,14 +32,18 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
     directions = vector_maker()
     
     # applying a vector on some cell
-    vector = directions['nw']
-    next_r = r + vector[0]
-    next_q = q + vector[1]  
+    #vector = directions['nw']
+    #next_r = r + vector[0]
+    #next_q = q + vector[1]  
     
     # accounting for infinite tiling
-    next_cell = bound_out(next_r, next_q)
+    #next_cell = bound_out(next_r, next_q)
     
-    print(next_cell)
+    #print(next_cell)
+    
+    # building on above, try applying a spread move
+    spread_move(board, r, q, board.get(cell[1]), 's')
+    print(board)
     
     actions = [
         (5, 6, -1, 1),
@@ -80,8 +82,14 @@ def vector_maker():
     return directions
 
 # function to handle the spread move   
-def spread_move(r, q, stack, dir):
+def spread_move(board, r, q, stack, dir):
     directions = vector_maker()
     
-    for i in range(1, stack):
-        
+    # 'kill' the cell we are spreading from
+    key = (r,q)
+    board.pop(key)
+    
+    
+    
+    print(board)
+    #for i in range(1, stack):
